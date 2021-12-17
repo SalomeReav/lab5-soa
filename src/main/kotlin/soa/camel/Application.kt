10a4c618b28364d9524b70186e260894a3e078c6
SalomeReav <salome.reav.50@gmail.com>
2021-12-17 21:40:18 +0100

    Update Application.kt
    Extend Query

--- src/main/kotlin/soa/camel/Application.kt
+++ src/main/kotlin/soa/camel/Application.kt
@@ -43,6 +43,11 @@ class Router(meterRegistry: MeterRegistry) : RouteBuilder() {
 
     override fun configure() {
         from(DIRECT_ROUTE)
+            .process { exchange ->
+                val q = exchange.getIn().getHeader("keywords") as String
+                val qF = q.replace("max:", "?count=")
+                exchange.getIn().setHeader("keywords", qF)
+            }
             .toD("twitter-search:\${header.keywords}")
             .wireTap(LOG_ROUTE)
             .wireTap(COUNT_ROUTE)
